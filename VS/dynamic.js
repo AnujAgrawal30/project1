1396c9c6c2542311f8613fae43a6d7f4a6fdbd1e
Anuj Agrawal <anujagrawal.official@gmail.com>
2020-06-11 21:10:47 +0530

    added jquery plugin

---
+++ VS/dynamic.js
@@ -0,0 +1,20 @@
+
+$(function () {
+    var reaction = $('.reacts'),
+        like = $('.like');
+    reaction.addClass('show');
+    like.on('click', function () {
+        if (reaction.hasClass('show')) {
+            reaction.css({
+                'display': 'none',
+            });
+            reaction.removeClass('show');
+        }
+        else {
+            reaction.css({
+                'display': 'flex',
+            });
+            reaction.addClass('show');
+        }
+    });
+});
